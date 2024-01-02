796ca940f008c51cc862857a4d1a9601c59abf89
Muhammad Ali <muhammadaliayaan295@gmail.com>
2024-01-02 14:12:45 +0500

    signup page functionality,UI,create middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,22 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) =>{
+export const signup = async (req, res, next) =>{
     const {username,email,password}= req.body;
     // store the comming information from server
     const hashedPassword = bcryptjs.hashSync(password, 10);
     // hide the password
     const newUser= new User({username, email, password: hashedPassword});
-    // save inside database
+    // save inside database encripted password
     try {
         await newUser.save()
-    // it takes time to save
-    res.status(201).json("user created successfully")
+        // it takes time to save
+        res.status(201).json("user created successfully")
     // something is created & message
     } catch (error) {
-        res.status(500).json(error.message);
-        // any error
-    }
+        next(error);
+        // any error send to json
+    };
     // if user entered against policy then arise error
-}
+};
